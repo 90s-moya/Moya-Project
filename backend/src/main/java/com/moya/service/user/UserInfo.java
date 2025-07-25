756f3a0813b1145ea84c81b5e4ee7529b5f1cb1e
김종수 <rlwhdtn97@naver.com>
2025-07-25 10:43:51 +0900

    [Feat]: project init
    resolves S13P11A602-39

---
+++ backend/src/main/java/com/moya/service/user/UserInfo.java
@@ -0,0 +1,28 @@
+package com.moya.service.user;
+
+import com.moya.domain.user.User;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
+public class UserInfo {
+
+	private Long userId;
+	private String username;
+	private LocalDateTime createAt;
+
+	@Builder
+	private UserInfo(Long userId, String username, LocalDateTime createAt) {
+		this.userId = userId;
+		this.username = username;
+		this.createAt = createAt;
+	}
+
+	public static UserInfo from(User user) {
+		return UserInfo.builder()
+			.userId(user.getId())
+			.username(user.getUsername())
+			.createAt(user.getCreatedAt())
+			.build();
+	}
+}
